e97f3da13d90675766c3711b397b254138416a9d
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2020-10-30 17:00:52 -0700

    Added login api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
-from rest_framework import status, viewsets,filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework import status, viewsets, filters
 
 from profiles_api import (
     serializers,
@@ -113,3 +115,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     # filters search fields by name or email
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    # generates a token when logging in
+    # adds the renderer classes to ObtainAuthToken view which enables it in the Django admin
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
